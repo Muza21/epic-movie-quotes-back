96d11e9a708d0eccb5bf1e4f4bbe59aca2e07a5f
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-30 16:44:09 +0400

    feat: add style to mail

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,58 +5,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Coronatime</title>
+    <title>Movie quotes</title>
 </head>
 <style>
-    main {
-        margin-top: 10%;
+    body {
+        background-color: #222030;
     }
 
     div {
-        margin-left: auto;
-        margin-right: auto;
         font-size: 18px;
         line-height: 18px;
         width: 343px;
     }
 
-    #confirm {
-        font-weight: 700;
+    h1 {
+        color: #DDCCAA;
     }
 
-    p {
-        text-align: center;
+
+    header {
+        width: 300px;
+        margin-left: auto;
+        margin-right: auto;
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
     }
 
-    a {
+
+
+    .container {
+        width: 1200px;
+        color: white;
+        margin: 100px auto;
+    }
+
+    .text {
+        margin-top: 32px;
+    }
+
+    #icon {
+        width: 30px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 80px;
+    }
+
+    #button {
         display: inline-block;
         padding: 1em 3em;
         border-radius: 5px;
         text-decoration: none;
-        color: white;
         margin: 0 auto;
-
-        background-color: #7ac142;
+        color: white;
+        background-color: #E31221;
     }
 
-    .button {
-        width: 100%;
-        text-align: center;
+    #link {
+        color: #DDCCAA;
+    }
+
+    @media all and (min-width:0px) and (max-width: 1000px) {
+        .container {
+            width: 100%;
+            color: white;
+            margin: 50px 5px
+        }
     }
 </style>
 
 <body>
     <main>
-        <div class="button">
-            <img src="" alt="coronatime">
-            <br><br>
-            <p style="text-align: center; color: black; font-weight: 700;">confirm email</p>
-            <p style="text-align: center; color: black;">confirm email</p>
-        </div>
-        <div class="button">
-            <a href="#">
-                confirm
-            </a>
+        <header>
+            <div id="icon">
+                <img src="{{ asset('assets/IconChat.svg') }}" alt="">
+
+            </div>
+            <h1>MOVIE QUOTES</h1>
+        </header>
+        <div class="container">
+            <p class="text">Hola dynamic name</p>
+
+            <p class="text">
+                Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your
+                account:
+            </p>
+
+            <div class="button">
+                <a id="button" href="#">
+                    Verify account
+                </a>
+            </div>
+            <p class="text">If clicking doesn't work, you can try copying and pasting it to your browser:</p>
+            <a id="link" href="#">some link</a>
+            <p class="text">If you have any problems, please contact us: support@moviequotes.ge</p>
+            <p class="text">MovieQuotes Crew</p>
         </div>
     </main>
 </body>
