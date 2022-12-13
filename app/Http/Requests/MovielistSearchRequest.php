8af2bdebb4a12ff6f2abf07ee5cebfd3973dd7a6
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-12-13 11:51:14 +0400

    fix: add rules

--- app/Http/Requests/MovielistSearchRequest.php
+++ app/Http/Requests/MovielistSearchRequest.php
@@ -14,7 +14,7 @@ class MovielistSearchRequest extends FormRequest
     public function rules()
     {
         return [
-            'text' => '',
+            'text' => 'required',
         ];
     }
 }
--- app/Http/Requests/NewsfeedSearchRequest.php
+++ app/Http/Requests/NewsfeedSearchRequest.php
@@ -14,7 +14,7 @@ class NewsfeedSearchRequest extends FormRequest
     public function rules()
     {
         return [
-            'text' => '',
+            'text' => 'required',
         ];
     }
 }
